fceb98994486f2db1d10625e4ba42efe7994c3b1
ferrarist <dvernitskiy.i.a@edu.mirea.ru>
2022-03-21 18:28:49 +0300

    done 03-reviews

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Review from './Review';
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <div className='container'>
+
+      <h1 className='reviews-header'>Our reviews</h1>
+      <Review />
+    </div>
+  )
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,53 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [currentPerson, setCurrentPerson] = useState(0)
+  const [person, setPerson] = useState(people[currentPerson])
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function clickForvard() {
+    if (currentPerson === people.length - 1) {
+      setCurrentPerson(0)
+      setPerson(people[currentPerson])
+    } else {
+      setCurrentPerson(prev => prev + 1)
+      setPerson(people[currentPerson])
+    }
+  }
+
+  function clickBackward() {
+    if (currentPerson === 0) {
+      setCurrentPerson(people.length - 1)
+      setPerson(people[currentPerson])
+    } else {
+      setCurrentPerson(prev => prev - 1)
+      setPerson(people[currentPerson])
+    }
+  }
+
+  function clickSurprise() {
+    setCurrentPerson(getRandomInt(people.length))
+    setPerson(people[currentPerson])
+  }
+
+  return (
+    <div className='review-wrapper'>
+      <img className='person-image' src={person.image}/>
+      <h2>{person.name}</h2>
+      <h4>{person.job}</h4>
+      <p>{person.text}</p>
+
+      <div className='toggle-btns-wrapper'>
+      <button className='fb-btns' onClick={clickBackward}>backwards</button>
+      <button className='fb-btns' onClick={clickForvard}>forvard</button>
+      </div>
+
+      <button className='random-btn' onClick={clickSurprise}>Surprise Me</button>
+    </div>
+  )
 };
 
 export default Review;
