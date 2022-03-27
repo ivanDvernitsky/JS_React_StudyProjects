2c5edff8eca9d329aedcd569d80a1df2b69d8f3b
ferrarist <dvernitskiy.i.a@edu.mirea.ru>
2022-03-27 16:38:57 +0300

    done 07-slider

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
+import { FaQuoteRight, FaRegCaretSquareDown } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [currPerson, setCurrPerson] = useState(0)
+  const person = data[currPerson]
+
+  function switchForward() {
+    if (currPerson === data.length - 1) {
+      setCurrPerson(0)
+    } else {
+      setCurrPerson(prev => prev + 1)
+    }
+  }
+  function switchBackward() {
+    if (currPerson === 0) {
+      setCurrPerson(data.length - 1)
+    } else {
+      setCurrPerson(prev => prev - 1)
+    }
+  }
+
+  useState(() => {
+    setInterval(switchForward, 10000)
+  }, [])
+
+  return (
+    <div className="container">
+      <h1 className="header">Reviews</h1>
+
+      <div className="content">
+
+      <button onClick={switchBackward}>B</button>
+
+      <div className="main">
+        <img className='person-image' src={person.image} />
+        <h2>{person.name}</h2>
+        <h3>{person.title}</h3>
+        <p>{person.quote}</p>
+      </div>
+
+      <button onClick={switchForward}>F</button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
