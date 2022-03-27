81a89dacda1eee83de6eda6c269edcf6bbc6eb77
ferrarist <dvernitskiy.i.a@edu.mirea.ru>
2022-03-27 17:13:29 +0300

    done 08-lorem-ipsum

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,31 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [paragraphs, setParagraphs] = useState(0)
+  const [submit, setSubmit] = useState(false)
   
-  
+  function toggleVal(event) {
+    setParagraphs(event.target.value)
+  }
+
+  const displayPar = data.map((par, index) => {
+    if (index < paragraphs) {
+      return <p className='parag' key={index}>{par}</p>
+    }
+  })
+
   return (
-  <h2>lorem ipsum project setup</h2>
+    <div className="container">
+      <h1 className="header">Tired of boring Lorem Ipsum?</h1>
+
+      <div className="input">
+        <label>Number of paragraphs: </label>
+        <input onClick={toggleVal} type='number' min='1' max='9'></input>
+        <button onClick={() => setSubmit(true)} >Submit</button>
+      </div>
+
+      {submit  ? displayPar : "" }
+    </div>
     )
 }
 
