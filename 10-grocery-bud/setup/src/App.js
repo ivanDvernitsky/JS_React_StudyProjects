e8c46a3ca5b86cbb31b0ba3d708a0154e62ed70f
ferrarist <dvernitskiy.i.a@edu.mirea.ru>
2022-03-27 22:38:27 +0300

    10 napolovinu

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,33 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [list, setList] = useState([])
+  const [listItem, setListItems] = useState('')
+
+  function onSubmit(event) {
+    event.preventDefault()
+    setList(prev => [...prev, listItem])
+    document.getElementById('prod-input').value = ''
+  }
+
+  function removeItem(num) {
+    setList(prev => prev.filter((item, index) => index != num))
+  }
+
+  return (
+    <div className="container">
+      <h1 className='header'>Grocery Bud</h1>
+
+      <form onSubmit={onSubmit}>
+        <input id="prod-input" type="text" onChange={(e) => setListItems(e.target.value)}/>
+        <button type='submit' >Submut</button>
+      </form>
+
+      <List list={list} removeItem={removeItem} />
+
+      {list.length > 1 ? <button id='clear-all'> Clear All</button> : <></>}
+    </div>
+  )
 }
 
 export default App
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({ list, removeItem }) => {
+  const displayList = list.map((item, index) => {
+    return <div className="list-item" key={index}>
+       <span> {item} </span>
+       <button onClick={() => removeItem(index)}>remove</button>
+       </div>
+  })
+
+  return (
+    <div className="list">
+      {displayList}
+    </div>
+  )
 }
 
 export default List
