77189350f2a28050cda530637440ebcc436a3ee5
ferrarist <dvernitskiy.i.a@edu.mirea.ru>
2022-03-27 20:57:25 +0300

    done 09-color-generator

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,48 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const defList = new Values('#f15025').all(10)
+
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(defList)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+      console.log(colors)
+    } catch (error) {
+      setError(true)
+    }
+  }
+
+  const displayColors = list.map((col, index) => {
+    return <SingleColor key={index} color={col} id={index}/>
+  })
+
+  return (
+    <section>
+      <div className='container'>
+        <h3>Color generator</h3>
+
+        <form onSubmit={handleSubmit}>
+          <input type="text" value = {color} 
+          onChange = {(e) => setColor(e.target.value)}
+          placeholder='#f15025'
+          style={{border: error ? '2px solid red' : 'none'}}
+          />
+          <button className="btn" type='submit'>Submit</button>
+        </form>
+      </div>
+
+      <div className="colors">
+        {displayColors}
+      </div>
+    </section>
+  )
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ color, id }) => {
+  const rgb = `#${color.rgb[0].toString(16)}${color.rgb[1].toString(16)}${color.rgb[2].toString(16)}`
+
+  const [copied, setCopied] = useState(false)
+
+  function copyColor() {
+    navigator.clipboard.writeText(rgb)
+    setCopied(true)
+  }
+  useEffect(() => {
+    setTimeout(() => setCopied(false), 3000)
+  }, [copied])
+
+  return (
+    <div className="single-color" 
+    onClick={copyColor}
+         style={{ backgroundColor: `rgb(${color.rgb[0]}, ${color.rgb[1]}, ${color.rgb[2]})`,
+                  color: id > 8 ? 'white' : 'black'
+                  }}>
+      <span>{rgb}</span> 
+      <br/>
+      <span className='percent'>{color.weight}% </span>
+      <br/>
+      <span className='copied-msg'>{copied ? 'Copied To clipboard' : ''}</span>
+    </div>
+    )
 }
 
 export default SingleColor
